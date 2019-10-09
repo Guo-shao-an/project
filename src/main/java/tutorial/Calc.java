3372c5215404500bd70fdcba008392f07d92811b
Guo-shao-an <a834194335@gmail.com>
2019-10-09 10:03:27 +0800

    first

---
+++ src/main/java/tutorial/Calc.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+public class Calc {
+    public static int add(int i, int j) {
+        return i + j;
+    }
+    public static int subtract(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/tutorial/AppTest.java
@@ -0,0 +1,42 @@
+package tutorial;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {   Calc c=new Calc();
+        int a=4;
+        int b=5;
+        int expected=a+b;
+        assertEquals(expected,c.add(a,b));
+        //assertTrue( true );
+    }
+}
